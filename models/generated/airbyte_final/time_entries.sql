e31bbc9271a5bfbb105bcec3c974d780002e16d3
AhmedAHassan <aaem.hassan@gmail.com>
2022-10-13 13:43:14 +0200

    feat merge into

--- models/generated/airbyte_final/time_entries.sql
+++ models/generated/airbyte_final/time_entries.sql
@@ -1,10 +1,15 @@
 {{ config(
-    unique_key = '_airbyte_ab_id',
-    tags = [ "top-level" ]
+    tags = [ "top-level" ],
+    materialized='incremental',
+        unique_key='id',
+        incremental_strategy='merge'
 ) }}
+
 -- Final base SQL model
 -- depends_on: {{ ref('entries') }}
-select
+WITH
+using_clause AS (
+    select
     id,
     type,
     tagids,
@@ -29,3 +34,36 @@ from {{ ref('entries') }} a
 left join {{ ref('entries_timeinterval') }} b
 on a._airbyte_entries_hashid = b._airbyte_entries_hashid
 -- time_entries from {{ source('dbo', '_airbyte_raw_entries') }}
+
+),
+
+
+updates AS (
+
+    SELECT
+        id,
+        type,
+        tagids,
+        taskid,
+        userid,
+        kioskid,
+        billable,
+        islocked,
+        projectid,
+        description,
+        workspaceid,
+        customfieldvalues,
+        duration,
+        {{ adapter.quote('end') }},
+        {{ adapter.quote('start') }},
+        _airbyte_entries_hashid,
+        _airbyte_ab_id,
+        _airbyte_emitted_at,
+        _airbyte_normalized_at,
+        _airbyte_timeinterval_hashid
+    FROM using_clause
+)
+
+SELECT *
+FROM updates
+
